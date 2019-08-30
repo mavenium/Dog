5a5136581ab0f2d733a3a214a187f2778d5a3e98
Mehdi Namaki <mavenium@gmail.com>
2019-08-31 01:40:13 +0430

    Create RequestQueue

---
+++ app/src/main/java/ir/mavenium/dog/RequestQueueContainer.java
@@ -0,0 +1,20 @@
+package ir.mavenium.dog;
+
+import android.content.Context;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+public class RequestQueueContainer {
+    private static RequestQueue requestQueue;
+
+    private RequestQueueContainer() {}
+
+    public static RequestQueue getInstance (Context context) {
+        if (requestQueue == null) {
+            requestQueue = Volley.newRequestQueue(context);
+        }
+
+        return requestQueue;
+    }
+}
